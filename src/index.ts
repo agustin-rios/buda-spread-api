adb3b28de02a7382da8325c73ed024d7176beea4
Agustin Rios <48336498+agustin-rios@users.noreply.github.com>
2024-06-10 04:29:42 +0000

    feat use .env to configure api PORT

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express, { Application } from "express";
-const PORT = 3000;
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+
 const app: Application = express();
+const PORT: number = parseInt(process.env.PORT ?? '8080');
+
 app.get('/', async (_req, res) => {
     res.send({
         message: `Server is running on port ${PORT}`,
     });
 });
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
