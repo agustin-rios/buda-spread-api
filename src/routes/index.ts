071ff8ef77e20af9f194075c92700aa72d39a1e7
agustin-rios <arios6@uc.cl>
2024-06-11 15:32:33 -0400

    feat add poll routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Express } from 'express';
 
 const budaMarketsRoute = require('./market');
 const alertRoute = require('./alert');
+const pollRoute = require('./poll');
 
 const applyRoutes = (app: Express, dir: string = '') => {
     app.use(`${dir}/buda/markets`, budaMarketsRoute);
     app.use(`${dir}/alert`, alertRoute);
+    app.use(`${dir}/poll`, pollRoute);
 };
 
 module.exports = applyRoutes;
